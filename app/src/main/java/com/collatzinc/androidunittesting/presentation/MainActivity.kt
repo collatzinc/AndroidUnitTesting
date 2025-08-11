293d54da586e54aa756831c8bc423dbe3ebd7fac
Milan Maji <milan.maji@callatzinc.com>
2025-08-11 18:34:42 +0530

    Implemented LoginScreen and ProfileScreen.

--- app/src/main/java/com/collatzinc/androidunittesting/presentation/MainActivity.kt
+++ app/src/main/java/com/collatzinc/androidunittesting/presentation/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.collatzinc.androidunittesting.ui.theme.AndroidUnitTestingTheme
+import com.collatzinc.androidunittesting.presentation.theme.AndroidUnitTestingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
